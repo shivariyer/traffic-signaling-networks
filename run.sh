0e8ff53c17b0118329188304b981fe38c710a8fd
Shiva R Iyer <rshiva06@gmail.com>
2022-04-11 02:00:30 -0700

    FIX: Check cmdline args in run.sh script

--- run.sh
+++ run.sh
@@ -6,7 +6,17 @@ scenario=$1
 # duration of the simulation in seconds
 simtime=$2
 
+# sanity check of commandline arguments
+if [ -z "$scenario" ] || [ -z "$simtime" ] ; then
+    echo "Please specify a scenario and simtime."
+    exit 1
+fi
+
 inpath=topologies/${scenario}.txt
+
+# check if file exists
+[[ ! -f "$inpath" ]] && { echo "Input topology file $inpath does not exist. Please try again."; exit 1; }
+
 outpath=output/${scenario}_sim.out
 
 echo "Starting simulation for $scenario for $simtime seconds ..."
